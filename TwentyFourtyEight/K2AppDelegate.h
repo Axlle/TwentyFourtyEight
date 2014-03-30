9c284d3aac1596c0d102f409ed878b5bb91cbd97
William Green <axlle99@gmail.com>
2014-03-30 15:18:53 -0700

    Initial commit

---
+++ TwentyFourtyEight/K2AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  K2AppDelegate.h
+//  TwentyFourtyEight
+//
+//  Created by William Green on 2014-03-30.
+//  Copyright (c) 2014 William Green. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface K2AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TwentyFourtyEight/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TwentyFourtyEight
+//
+//  Created by William Green on 2014-03-30.
+//  Copyright (c) 2014 William Green. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "K2AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([K2AppDelegate class]));
+    }
+}
